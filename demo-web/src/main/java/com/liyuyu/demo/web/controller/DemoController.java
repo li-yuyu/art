4d111db506cce32db9dbbc0db964f44ebdd97605
liyuyu <liyuyu@pinjamango.com>
2020-03-04 16:05:44 +0800

    swagger2

--- demo-web/src/main/java/com/liyuyu/demo/web/controller/DemoController.java
+++ demo-web/src/main/java/com/liyuyu/demo/web/controller/DemoController.java
@@ -18,8 +18,14 @@ import com.liyuyu.demo.web.result.ResponseResult;
 import com.liyuyu.demo.web.service.DemoService;
 import com.liyuyu.demo.web.vo.DemoVO;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/demo")
+@Api(tags = "示例模块")
 public class DemoController {
 
 	@Autowired
@@ -32,14 +38,17 @@ public class DemoController {
 	}
 
 	@PostMapping("/post")
+	@ApiOperation(value = "post测试")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "name", value = "姓名", required = true, example = "liyuyu"), })
 	public ResponseResult<PageResult<DemoVO>> post(@RequestBody @Valid DemoDTO dto, BindingResult validResult) {
 		if (validResult.hasErrors()) {
-			return ResponseResult.error(ErrorCodeEnum.ILLEGAL_PARAM.getCode(), validResult.getFieldError().getDefaultMessage());
+			return ResponseResult.error(ErrorCodeEnum.ILLEGAL_PARAM.getCode(),
+					validResult.getFieldError().getDefaultMessage());
 		}
 		PageResult<DemoVO> result = demoService.listByPage(dto);
 		return ResponseResult.success(result);
 	}
-	
+
 	@GetMapping("/exception")
 	public ResponseResult<String> exception() {
 		Integer.parseInt("abc");
